7c9c4f5ac2cc342dd241df6773fb88c381fede5a
MinexAutomation <minex.automation@gmail.com>
2020-01-19 17:42:46 -0800

    Added a default human output file name provider implementation with a hard-coded file name.

---
+++ source/R5T.Bath.File.Default/Code/Services/Implementations/DefaultHumanOutputFileNameProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Bath.File.Default
+{
+    public class DefaultHumanOutputFileNameProvider : IHumanOutputFileNameProvider
+    {
+        public const string FileName = "Human Output.txt";
+
+
+        public string GetHumanOutputFileName()
+        {
+            return DefaultHumanOutputFileNameProvider.FileName;
+        }
+    }
+}

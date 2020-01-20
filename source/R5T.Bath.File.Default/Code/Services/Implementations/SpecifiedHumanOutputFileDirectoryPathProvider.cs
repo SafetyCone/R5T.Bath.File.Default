6270917c1865138878cc13a00181b1fb8808f3ac
MinexAutomation <minex.automation@gmail.com>
2020-01-19 16:50:28 -0800

    Added specified implementations of human output file path related services.

---
+++ source/R5T.Bath.File.Default/Code/Services/Implementations/SpecifiedHumanOutputFileDirectoryPathProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace R5T.Bath.File.Default
+{
+    public class SpecifiedHumanOutputFileDirectoryPathProvider : IHumanOutputFileDirectoryPathProvider
+    {
+        private string HumanOutputFileDirectoryPath { get; }
+
+
+        public SpecifiedHumanOutputFileDirectoryPathProvider(string humanOutputFileDirectoryPath)
+        {
+            this.HumanOutputFileDirectoryPath = humanOutputFileDirectoryPath;
+        }
+
+        public string GetHumanOutputFileDirectoryPath()
+        {
+            return this.HumanOutputFileDirectoryPath;
+        }
+    }
+}
---
+++ source/R5T.Bath.File.Default/Code/Services/Implementations/SpecifiedHumanOutputFileNameProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace R5T.Bath.File.Default
+{
+    public class SpecifiedHumanOutputFileNameProvider : IHumanOutputFileNameProvider
+    {
+        private string HumanOutputFileName { get; }
+
+
+        public SpecifiedHumanOutputFileNameProvider(string humanOutputFileName)
+        {
+            this.HumanOutputFileName = humanOutputFileName;
+        }
+
+        public string GetHumanOutputFileName()
+        {
+            return this.HumanOutputFileName;
+        }
+    }
+}

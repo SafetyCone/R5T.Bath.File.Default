b0e0eb615d6111281fa5a3dc91e8a2fd2916f3f0
MinexAutomation <minex.automation@gmail.com>
2020-01-19 16:08:04 -0800

    Added a default IHumanOutputFilePathProvider implementation that just pushes the work of providing the file path to an IHumanOutputFileDirectoryPathProvider and IHumanOutputFileNameProvider service (and using the IStringlyTypedPathOperator service).

---
+++ source/R5T.Bath.File.Default/Code/Services/Implementations/DefaultHumanOutputFilePathProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using R5T.Lombardy;
+
+
+namespace R5T.Bath.File
+{
+    public class DefaultHumanOutputFilePathProvider : IHumanOutputFilePathProvider
+    {
+        private IHumanOutputFileDirectoryPathProvider HumanOutputFileDirectoryPathProvider { get; }
+        private IHumanOutputFileNameProvider HumanOutputFileNameProvider { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public DefaultHumanOutputFilePathProvider(
+            IHumanOutputFileDirectoryPathProvider humanOutputFileDirectoryPathProvider,
+            IHumanOutputFileNameProvider humanOutputFileNameProvider,
+            IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.HumanOutputFileDirectoryPathProvider = humanOutputFileDirectoryPathProvider;
+            this.HumanOutputFileNameProvider = humanOutputFileNameProvider;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetHumanOutputFilePath()
+        {
+            var directoryPath = this.HumanOutputFileDirectoryPathProvider.GetHumanOutputFileDirectoryPath();
+            var fileName = this.HumanOutputFileNameProvider.GetHumanOutputFileName();
+
+            var filePath = this.StringlyTypedPathOperator.GetFilePath(directoryPath, fileName);
+            return filePath;
+        }
+    }
+}

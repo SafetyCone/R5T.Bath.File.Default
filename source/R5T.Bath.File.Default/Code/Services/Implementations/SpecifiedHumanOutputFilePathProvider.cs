ac53eb1bb614efd52c09d1b78e29470df74677cd
MinexAutomation <minex.automation@gmail.com>
2020-01-19 16:04:57 -0800

    Added a direct-specification IHumanOutputFilePathProvider implementation.

---
+++ source/R5T.Bath.File.Default/Code/Services/Implementations/SpecifiedHumanOutputFilePathProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace R5T.Bath.File
+{
+    public class SpecifiedHumanOutputFilePathProvider : IHumanOutputFilePathProvider
+    {
+        private string HumanOutputFilePath { get; }
+
+
+        public SpecifiedHumanOutputFilePathProvider(string humanOutputFilePath)
+        {
+            this.HumanOutputFilePath = humanOutputFilePath;
+        }
+
+        public string GetHumanOutputFilePath()
+        {
+            return this.HumanOutputFilePath;
+        }
+    }
+}
